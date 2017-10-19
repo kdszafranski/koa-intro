6e583f075b768aa8335d28adef93b3d6724d12b8
Kris Szafranski <kdszafranski@gmail.com>
2017-10-19 15:52:27 -0500

    basic koa server

---
+++ server.js
@@ -0,0 +1,27 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const app = new Koa();
+const router = new Router();
+const port = 5000;
+
+
+// middleware
+app.use((ctx, next) => {
+    console.log('hi');
+    next();
+});
+
+// routes
+router.get('/koala', function(ctx, next) {
+  ctx.response.body = 'yo, holla!';
+});
+
+
+// route configuration
+app.use(router.routes());
+// console.log('routes: ', router.routes());
+app.use(router.allowedMethods());
+
+app.listen(port, function() {
+  console.log('look at port: ', port);
+});
